72ccc8aa0dc74d915526d70a0bdceb283531bd07
rina724 <rinarin1024@gmail.com>
2024-09-09 13:31:58 +0900

    ログイン後に化粧品一覧に遷移する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   # ログイン後のリダイレクト先
   def after_sign_in_path_for(resource_or_scope)
     # ここを好きなパスに変更
-    root_path
+    cosmetics_path
   end
 
   # ログアウト後のリダイレクト先
