d6d05ffcacaf1c1eae7c59acbdc88b4c386bbe65
rina724 <rinarin1024@gmail.com>
2024-09-18 15:28:17 +0900

    マイページ機能の作成

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,41 +1,40 @@
 class ProfilesController < ApplicationController
+  def update
+    @user = current_user
+    @current_user_profile = Profile.find(params[:id])
+    if @current_user_profile.update(profile_params)
+      redirect_to profiles_path
+    else
+      render :update, status: :unprocessable_entity
+    end
+  end
+
   def index
     @user = current_user
-    @profile = @user.profile || Profile.new(user: @user)
-    @allergies = Profile.includes(:user)
-    
-    # Get bad mycosmetics for the user
+    @current_user_profile = @user.profile || @user.build_profile
+
+    # ユーザーが登録したマイコスメでバッドを選んだものだけ取得
     @user_bad_mycosmetics = Mycosmetic.joins(:cosmetic).where(mycosmetics: { problem: 2 }, user_id: @user.id)
-    
-    # Count ingredients
+
+    # 成分をカウントする
     ingredient_count = Hash.new(0)
-    
     @user_bad_mycosmetics.each do |my_cosmetic|
       cosmetic = my_cosmetic.cosmetic
-      ingredients = cosmetic.ingredient.split(',')
-      
+      ingredients = cosmetic.ingredient.split(",")
       ingredients.each do |ingredient|
         ingredient_count[ingredient.strip] += 1
       end
     end
-    
-    # Set caution ingredients (5 times or more)
-    @caution_ingredients = ingredient_count.select { |_, count| count >= 3 }.keys
-    @profile.save
-    # Update profile with caution ingredients
-    if @profile.update(caution: @caution_ingredients.join(', '))
-      flash[:notice] = "Caution ingredients updated successfully."
-    else
-      flash[:alert] = "Failed to update caution ingredients."
-    end
-    
-    # Render the view
-    render :index
+
+    # ５回重複している成分
+    @caution_ingredients = ingredient_count.select { |_, count| count >= 5 }.keys
+    # プロフィールの注意する成分を上書き
+    @current_user_profile && @current_user_profile.update(caution: @caution_ingredients.join(", "))
   end
 
   private
 
   def profile_params
-    params.require(:profile).permit(:caution, :allergy)
+    params.require(:profile).permit(:allergy)
   end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,4 @@
 class Profile < ApplicationRecord
-  belongs_to :mycosmetic
+  belongs_to :mycosmetic, optional: true
   belongs_to :user
 end
