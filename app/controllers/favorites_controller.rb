0ecad839326f64220bc491ea4319dd7ddab789a3
rina724 <rinarin1024@gmail.com>
2024-09-21 23:09:13 +0900

    修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,12 +2,10 @@ class FavoritesController < ApplicationController
   def create
     @cosmetic = Cosmetic.find(params[:cosmetic_id])
     current_user.favorite(@cosmetic)
-    redirect_to request.referer
   end
 
   def destroy
     @cosmetic = Cosmetic.find(params[:cosmetic_id])
     current_user.unfavorite(@cosmetic)
-    redirect_to request.referer, status: :see_other
   end
 end
