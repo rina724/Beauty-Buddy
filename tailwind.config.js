32c7996f51cfa4ec50c7d3131dacf41a67c3f7ea
rina724 <rinarin1024@gmail.com>
2024-10-02 08:35:04 +0900

    tailwind.config.jsを修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,24 +3,37 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js',
+    './app/javascript/**/*.js'
   ],
   theme: {
     extend: {
       fontFamily: {
         abril: "Abril Fatface",
-        gen: "Gen Jyuu GothicX",
+        gen: "Gen Jyuu GothicX"
       },
-    },
-    colors: {
-      'cream': '#FFFBEC',
-      'beige': '#EDDDC5',
-      'mint': '#94D0BF',
-      'yellow': '#FFE567',
-      'gray': '#f3f4f6',
-      'green': '#86efac',
-      'red': '#fca5a5',
-    },
+      colors: {
+        'cream': '#FFFBEC',
+        'beige': '#EDDDC5',
+        'mint': '#94D0BF',
+        'yellow': '#FFE567',
+        'gray': '#f3f4f6',
+        'green': '#86efac',
+        'red': '#fca5a5'
+      },
+    }
   },
   plugins: [require("daisyui")],
-};
+  daisyui: {
+    themes: [
+      {
+        mytheme: { 
+          "primary": "#a991f7",
+          "secondary": "#f6d860",
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-100": "#ffffff",
+          }
+      }
+    ]
+  }
+}
