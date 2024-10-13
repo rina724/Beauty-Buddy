7ca62fcbd14ec8094805b805777ccf3a7c5925ac
rina724 <rinarin1024@gmail.com>
2024-10-14 02:27:51 +0900

    cosmeticテーブルからamountとingredientカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_05_160816) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_13_172628) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,8 +30,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_05_160816) do
 
   create_table "cosmetics", force: :cascade do |t|
     t.string "product_name", null: false
-    t.integer "amount", null: false
-    t.string "ingredient", null: false
     t.string "image", null: false
     t.bigint "category_id"
     t.bigint "brand_id"
