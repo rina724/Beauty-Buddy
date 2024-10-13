aaee1e07599c2718baa5b74df9fbd9a1e4976f02
rina724 <rinarin1024@gmail.com>
2024-10-14 02:29:40 +0900

    ingredientテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_13_172628) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_13_172808) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,6 +71,13 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_13_172628) do
     t.datetime "updated_at", null: false
     t.index ["cosmetic_id"], name: "index_favorites_on_cosmetic_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_ingredients_on_name", unique: true
   end
 
   create_table "mycosmetics", force: :cascade do |t|
