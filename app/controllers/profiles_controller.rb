5326c4f3ccdd5ccb4ba1d9634abc50a260e3f5ff
rina724 <rinarin1024@gmail.com>
2024-11-02 01:23:29 +0900

    全てのチェックボックスを外してもせチェックボックスを外しても成分名ボックスを外しても成分名が表外しても成分名が表記され成分名が表記される

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,7 +5,7 @@ class ProfilesController < ApplicationController
     
     # バッド評価のマイコスメを取得
     @user_bad_mycosmetics = Mycosmetic.joins(:cosmetic)
-                                     .where(mycosmetics: { problem: 2 }, user_id: @user.id)
+                                      .where(mycosmetics: { problem: 2 }, user_id: @user.id)
     
     # 成分のカウントと頻出成分の取得
     @ingredient_counts = count_ingredients
@@ -37,20 +37,22 @@ class ProfilesController < ApplicationController
   private
 
   def update_ingredients
-    begin
-      # 現在の関連をすべて削除
-      @current_user_profile.profiles_ingredients.destroy_all
-      
-      # 選択された成分の関連を作成
-      if params[:ingredient_ids].present?
-        params[:ingredient_ids].reject(&:blank?).each do |ingredient_id|
-          @current_user_profile.profiles_ingredients.create!(
-            ingredient_id: ingredient_id
-          )
-          end
-        end
+    # 現在の関連をすべて削除
+    @current_user_profile.profiles_ingredients.destroy_all
+    
+    # 選択された成分の関連を作成
+    if params[:ingredient_ids].present? && params[:ingredient_ids].any?
+      params[:ingredient_ids].reject(&:blank?).each do |ingredient_id|
+        @current_user_profile.profiles_ingredients.create!(
+          ingredient_id: ingredient_id
+        )
       end
+    else
+      # 何も選択されていない場合、関連を空にしない
+      return true
+    end
   end
+
   def count_ingredients
     return Hash.new(0) if @user_bad_mycosmetics.nil?
     
@@ -67,13 +69,12 @@ class ProfilesController < ApplicationController
     return [] if @ingredient_counts.empty?
     
     # 出現回数が2回以上の成分を抽出
-    # または、必要に応じて閾値を調整
     threshold = 2
     
     Ingredient.where(id: @ingredient_counts.select { |ingredient, count| 
-      count >= threshold 
-    }.keys.map(&:id))
+      count >= threshold }.keys.map(&:id))
   end
+
   def profile_params 
     params.require(:profile).permit(:allergy) 
   end
